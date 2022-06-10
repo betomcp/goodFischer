2a0142bce636f8529968fe8463f1a00adffbc6bd
Brunoporto2702 <brunop2702@gmail.com>
2022-06-10 18:11:32 -0300

    COmmit inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+
+let app = express();
+let __dirname = path.resolve();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log(req.hostname)
+    res.sendFile(path.join(__dirname + "/index.html"));
+})
+
+app.post("/", (req, res) => {
+    res.json(req.body);
+})
+
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
