cc2e2013dbda12034ba91afc4409f0a8484e9437
betomcp <beto.porto2000@gmail.com>
2022-06-27 01:03:13 -0300

    s

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,28 +1,79 @@
-let projects = document.getElementsByClassName('testes_p')
-let mcImg1 = document.getElementById('mc-1-id')
 
-let mcImgHeight = mcImg1.offsetTop;
-
-let divTMc = document.getElementById('div-title-mc')
+let p1 = document.getElementsByClassName('p_1')
+let p2 = document.getElementsByClassName('p_2')
+let p3 = document.getElementsByClassName('p_3')
 
 
-// window.onload = ()=>{
-//     mcImg1.addEventListener('click', ()=>{
-//         // location('http://localhost:3000/web-motors')
-//         alert('salve')
-//     })
-// }
-divTMc.addEventListener('click', ()=>{
+//VIGOR
+
+let divVigor = document.getElementById('div-vigor')
+
+divVigor.addEventListener('click', ()=>{
     window.location = 'http://localhost:3000/web-motors'
-    // alert('salve')
     // window.open('http://localhost:3000/web-motors')
 })
 
+window.onload = ()=>{
+    let n = window.innerWidth / 187.5
+    let s = n.toString()
+    console.log(s)
+    let n2 = n * 0.375
+    let s2 = n2.toString() 
 
-// let teste = 3;
+    p1[0].style.fontSize = s + 'rem'
+    p2[0].style.fontSize = s2 + 'rem'
+    p3[0].style.fontSize = s2 + 'rem'
+    p1[1].style.fontSize = s + 'rem'
+    p2[1].style.fontSize = s2 + 'rem'
+    p3[1].style.fontSize = s2 + 'rem'
+    p1[2].style.fontSize = s + 'rem'
+    p2[2].style.fontSize = s2 + 'rem'
+    p3[2].style.fontSize = s2 + 'rem'
+    p1[3].style.fontSize = s + 'rem'
+    p2[3].style.fontSize = s2 + 'rem'
+    p3[3].style.fontSize = s2 + 'rem'
+    p1[6].style.fontSize = s + 'rem'
+    p2[6].style.fontSize = s2 + 'rem'
+    p3[6].style.fontSize = s2 + 'rem'
+    p1[7].style.fontSize = s + 'rem'
+    p2[7].style.fontSize = s2 + 'rem'
+    p3[7].style.fontSize = s2 + 'rem'
+    p1[9].style.fontSize = s + 'rem'
+    p2[9].style.fontSize = s2 + 'rem'
+    p3[9].style.fontSize = s2 + 'rem'
+}
 
-// console.log(mcImgHeight.toString() + 'px')
+window.onresize = ()=>{
+    
+    let n = window.innerWidth / 187.5
+    let s = n.toString()
+    let n2 = n * 0.375
+    let s2 = n2.toString() 
 
-// projects[0].style.top = mcImgHeight.toString() + 'px'
-// projects[0].style.top = '-50vh'
+    p1[0].style.fontSize = s + 'rem'
+    p2[0].style.fontSize = s2 + 'rem'
+    p3[0].style.fontSize = s2 + 'rem'
+    p1[1].style.fontSize = s + 'rem'
+    p2[1].style.fontSize = s2 + 'rem'
+    p3[1].style.fontSize = s2 + 'rem'
+    p1[2].style.fontSize = s + 'rem'
+    p2[2].style.fontSize = s2 + 'rem'
+    p3[2].style.fontSize = s2 + 'rem'
+    p1[3].style.fontSize = s + 'rem'
+    p2[3].style.fontSize = s2 + 'rem'
+    p3[3].style.fontSize = s2 + 'rem'
+    p1[6].style.fontSize = s + 'rem'
+    p2[6].style.fontSize = s2 + 'rem'
+    p3[6].style.fontSize = s2 + 'rem'
+    p1[7].style.fontSize = s + 'rem'
+    p2[7].style.fontSize = s2 + 'rem'
+    p3[7].style.fontSize = s2 + 'rem'
+    p1[9].style.fontSize = s + 'rem'
+    p2[9].style.fontSize = s2 + 'rem'
+    p3[9].style.fontSize = s2 + 'rem'
+}
+
+
+
+
 
